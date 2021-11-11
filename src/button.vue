743f223f5a781608fe249bff1b4d1b27ca62cef7
sansui-d <792703157@qq.com>
2021-11-11 21:43:24 +0800

    button

---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="Tbutton">按钮</button>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="sass" scoped>
+.Tbutton
+    height: var(--button-height)
+    font-size: var(--font-size)
+    padding: 0 1em
+    border-radius: var(--button-radius)
+    border: 1px solid var(--border-color)
+    background: var(--button-bg)
+
+    &:hover
+        border-color: var(--border-color-hover)
+
+    &:active
+        background-color: var(--button-active-bg)
+
+    &:focus
+        outline: none
+</style>
---
+++ src/vue.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Button from './button.vue'
+Vue.component('Tbutton',Button)
+new Vue({
+    el:'#app',
+})
